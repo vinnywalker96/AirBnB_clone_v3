3f92869a26916fa514090ddf1f847a270edf3b4a
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-06-07 22:36:54 +0000

    document modules

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""import libraries"""
 from api.v1.views import app_views
 from flask import  jsonify
 from models.amenity import Amenity
@@ -11,10 +12,12 @@ from models import storage
 
 @app_views.route('/status', methods=['GET'])
 def status():
+    """ get status"""
     return jsonify({"status": "ok"})
 
 @app_views.route('/stats', methods=['GET'])
 def stats():
+    """get stats"""
     stat = {
             "amenities": storage.count(Amenity),
             "cities": storage.count(City),
--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,9 +2,13 @@
 from api.v1.views import app_views
 from models.state import State
 from models import storage
+from flask import jsonify
 
 @app_views.route('/states', methods=['GET'])
 def get_states():
     """retrieves states"""
     states = list(storage.all(State).values())[0].id
-    return states
+    data = list(storage.get(State, states).to_dict())
+    return jsonify(data)
+
+
