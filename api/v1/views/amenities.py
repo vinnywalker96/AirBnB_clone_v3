eab21732e4b9160343b8c3a74e68ff778d103e37
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-06-09 23:36:13 +0000

    Get allAmenities

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -12,7 +12,7 @@ from flask import abort, request, make_response
 def get_amenity():
     """Get all amenities"""
     amenities = storage.all(Amenity)
-    amenity_list = [amenity.to_dict() 
+    amenity_list = [amenity.to_dict()
                     for amenity in amenities.values()]
     return jsonify(amenity_list)
 
@@ -24,6 +24,51 @@ def get_amenities(amenity_id):
     amenities = storage.get(Amenity, amenity_id)
     if amenity is None:
         abort(404)
-    amenity_list = [amenity.to_dict() 
+    amenity_list = [amenity.to_dict()
                     for amenity in amenities.value()]
     return jsonify(amenity_list)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_amenity(amenity_id):
+    """Deletes an amenity"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    storage.delete(amenity)
+    storage.new(amenity)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/amenities', methods=['POST'],
+                 strict_slashes=False)
+def post_amenity():
+    """Posts a new amenity"""
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    amenity = Amenity(**data)
+    amenity.save()
+    return make_response(jsonify(amenity.to_dict()), 201)
+
+
+@app_views.route('/amenities/<amenity_id>', methods=['PUT'],
+                 strict_slashes=False)
+def put_amenity(amenity_id):
+    """Updates an amenity"""
+    amenity = storage.get(Amenity, amenity_id)
+    if amenity is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    ignore_keys = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(amenity, key, value)
+    amenity.save()
+    return make_response(jsonify(amenty.to_dict()), 200)
