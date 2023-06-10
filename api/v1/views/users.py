cde4ad6977f8e2eab843b178e97aabcc27d64881
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-06-10 12:13:47 +0000

    Get all users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""Import Libraries of user"""
+"""Import Libraries for Users"""
 from api.v1.views import app_views
 from flask import jsonify
 from flask import abort
-from flask import request, make_response
+from flask import request
+from flask import make_response
 from models import storage
 from models.user import User
 
 
-@app_views.route('/users'/, methods=['GET'],
-                 strict_slashes=False)
+@app_view.route('/users', methods=['GET'],
+                strict_slashes=False)
 def get_users():
-    """Retrieves  all the users"""
+    """Get all Users"""
     users = storage.all(User)
-    user_list = [user.to_dict() for user in users.values()]
+    user_list = [user.to_dict() for user in users.value()]
     return jsonify(user_list)
-
-
-@app_views.route('/users/<user_id>', methods=['GET'],
-                 strict_slashes=False)
-def get_user_by_id(user_id):
-    """Retrieves all the users"""
-    users = storage.get(User, user_id)
-    if users is None:
-        abort(404)
-    return jsonify(users.to_dict())
