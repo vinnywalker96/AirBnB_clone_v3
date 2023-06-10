98c34a9155f6d7691a535663918d2cd8874a1abc
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-06-10 13:41:22 +0000

    Get all users

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -10,7 +10,7 @@ from models.user import User
 
 
 @app_views.route('/users', methods=['GET'],
-                strict_slashes=False)
+                 strict_slashes=False)
 def get_users():
     """Get all Users"""
     users = storage.all(User)
@@ -39,3 +39,41 @@ def delete_user(user_id):
     storage.save()
     return make_response(jsonify({}), 200)
 
+
+@app_views.route('/users', methods=['POST'],
+                 strict_slashes=False)
+def create_user():
+    """Creates a new user"""
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    if 'email' not in data:
+        abort(400, 'Missing email')
+    if 'password' not in data:
+        abort(400, 'Missing password')
+    user = User(**data)
+    user.save()
+    return make_response(jsonify(user.to_dict()), 201)
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_user(user_id):
+    """Updates user """
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    ignore_keys = [
+            'id',
+            'email',
+            'created_at',
+            'updated_at'
+            ]
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(user, key, value)
+    user.save()
+    return make_response(jsonify(user.to_dict()), 200)
