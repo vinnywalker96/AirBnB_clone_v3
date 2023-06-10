ad6e573fdb308aa7973ce8eee50c0bccc7185178
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-06-10 14:16:28 +0000

    add places

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -8,6 +8,8 @@ from flask import make_response
 from models import storage
 from models.place import Place
 from models.city import City
+from models.user import User
+
 
 @app_views.route('/cities/<city_id>/places', methods=['GET'],
                  strict_slashes=False)
