e424a21449bdc114aa3de5efa1e2fa008fa66755
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-06-11 11:01:11 +0000

    fix place url

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -41,3 +41,53 @@ def delete_place(place_id):
     storage.delete(place)
     storage.save()
     return make_response(jsonify({}), 200)
+
+
+@app_views.route('/cities/<city_id>/<place_id>', methods=['POST'],
+                 strict_slashes=False)
+def create_place(city_id):
+    """Creates a new city"""
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    if 'user_id' not in data:
+        abort(400, 'Missing user_id')
+    user = storage.get(User, data['user_id'])
+    if user is None:
+        abort(404)
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    place = Place()
+    place.name = data['name']
+    place.city_id = city_id
+    storage.new(place)
+    storage.save()
+    return make_response(jsonify(place.to_dict()), 201)
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_place(place_id):
+    """Update place"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    ignore_keys = [
+            'id',
+            'user_id',
+            'city_id',
+            'created_at',
+            'updated-at',
+            ]
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(place, key, value)
+    place.save()
+    return make_response(jsonify(place.to_dict()), 200)
+
