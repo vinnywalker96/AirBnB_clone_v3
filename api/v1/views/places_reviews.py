b25420e76cbb9a2199074a3ff2f2b3a9aa7feeb1
Marlvin Kwenda <vinnywalker96@icloud.com>
2023-06-12 12:49:48 +0000

    Reviews

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -23,7 +23,6 @@ def get_reviews(place_id):
     return jsonify(reviews)
 
 
-
 @app_views.route('/reviews/<review_id>', methods=['GET'],
                  strict_slashes=False)
 def get_review(review_id):
@@ -32,3 +31,59 @@ def get_review(review_id):
     if reviews is None:
         abort(404)
     return jsonify(reviews.to_dict())
+
+
+@app_views.route('/reviews/<review_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_review(review_id):
+    """Delete a review"""
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+    storage.delete(review)
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/places/<place_id>/reviews', methods=['POST'],
+                 strict_slashes=False)
+def post_review(place_id):
+    """Post a review"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    user = Storage.get(User, data['user_id'])
+    if 'user_id' not in data:
+        abort(400, 'Missing user_id')
+    if 'text' not in data:
+        abort(400, 'Missing text')
+    review = Review(**data)
+    review.save()
+    return make_response(jsonify(review.to_dict()), 201)
+
+
+@app_views.route('/reviews/<review_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_review(review_id):
+    """Update a Review"""
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    ignore_keys = [
+            'id',
+            'user_id',
+            'place_id',
+            'created_at',
+            'updated_at'
+            ]
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(review, key, value)
+    review.save()
+    return make_response(jsonify(review.to_dict()), 200)
